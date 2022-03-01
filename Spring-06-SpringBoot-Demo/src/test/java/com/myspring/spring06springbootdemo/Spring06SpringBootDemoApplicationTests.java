9131d2ed8bcd00698a6624d0c11442d3ab119895
adam0nur <adamnur0117@gmail.com>
2022-02-28 21:31:02 -0600

    Learing update 02

---
+++ Spring-06-SpringBoot-Demo/src/test/java/com/myspring/spring06springbootdemo/Spring06SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspring.spring06springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring06SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
