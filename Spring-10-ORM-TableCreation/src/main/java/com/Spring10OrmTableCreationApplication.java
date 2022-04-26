659b78142683e9c686ce77202289c52ad76673a5
adam0nur <adamnur0117@gmail.com>
2022-04-25 19:42:27 -0500

    DD_connection and annotations

--- Spring-10-ORM-TableCreation/src/main/java/com/Spring10OrmTableCreationApplication.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/Spring10OrmTableCreationApplication.java
@@ -1,4 +1,4 @@
-package com.example.spring10ormtablecreation;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
