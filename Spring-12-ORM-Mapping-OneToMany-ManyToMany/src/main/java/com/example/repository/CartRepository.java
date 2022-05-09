13ae90d37bb985eb9b04a0254eaa348c83506ec1
adam0nur <adamnur0117@gmail.com>
2022-05-08 21:58:40 -0500

    Day 12. DB Relations

---
+++ Spring-12-ORM-Mapping-OneToMany-ManyToMany/src/main/java/com/example/repository/CartRepository.java
@@ -0,0 +1,13 @@
+package com.example.repository;
+
+import com.example.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface CartRepository extends JpaRepository<Cart,Long> {
+
+}
+
+
+
