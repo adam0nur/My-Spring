7082cef8b46959796d536d8a3ff3773b1c5ecdd2
adam0nur <adamnur0117@gmail.com>
2022-05-14 21:30:14 -0500

    day 13 - and day 14 in progress->

---
+++ Spring-14-ORM-Queries/src/main/java/com/example/repository/RegionRepository.java
@@ -0,0 +1,8 @@
+package com.example.repository;
+
+import com.example.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region,Long> {
+
+}
