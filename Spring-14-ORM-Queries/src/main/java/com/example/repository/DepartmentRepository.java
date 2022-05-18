0fcccb071e399181a04d761d3d6de7a00beb870e
adam0nur <adamnur0117@gmail.com>
2022-05-18 15:50:06 -0500

    day 20 - and day 21 in progress->

---
+++ Spring-14-ORM-Queries/src/main/java/com/example/repository/DepartmentRepository.java
@@ -0,0 +1,26 @@
+package com.example.repository;
+
+
+import com.example.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface DepartmentRepository extends JpaRepository<Department,String> {
+
+    //Display all Departments in the furniture department
+    List<Department> findByDepartment(String department);
+
+    //Display all Departments in the Health Division
+    List<Department> findByDivisionIs(String division);
+
+    //Display top 3 departments with division name includes 'Hea', without duplicates
+    List<Department> findDistinctTop3ByDivisionContains(String pattern);
+
+
+
+
+
+
+}
